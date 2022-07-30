1089996eea21b6f1db163dad134e63334f5c6d17
Adriana Cavalcanti <adriana.cavalcanti@cmd.com.au>
2022-07-30 15:54:05 +1000

    -

--- handler/handler.py
+++ handler/handler.py
@@ -1,3 +1,4 @@
+import argparse
 import yaml
 
 def read_yaml(file):
@@ -6,3 +7,11 @@ def read_yaml(file):
             print(yaml.safe_load(stream))
         except yaml.YAMLError as exc:
             print(exc)
+
+
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser(description='SECRETS_ENV_VARS.')
+    parser.add_argument('SECRETS_ENV_VARS',
+                    help='secrets from github secrets')
+else: 
+    print ("File1 is being imported")
